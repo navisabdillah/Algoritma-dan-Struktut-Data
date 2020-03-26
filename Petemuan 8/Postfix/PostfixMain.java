020cba44a1ff0110f1892a18d0ac3c9c5889281c
navisabdillah <60720757+navisabdillah@users.noreply.github.com>
2020-03-26 13:31:09 +0700

    Add files via upload

---
+++ Petemuan 8/Postfix/PostfixMain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu.pkg8;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Navis
+ */
+public class PostfixMain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String P, Q;
+        System.out.println("Masukkan ekspresi matematika: ");
+        Q = sc.nextLine();
+        Q = Q.trim();
+        Q = Q + ")";
+
+        int total = Q.length();
+        Postfix post = new Postfix(total);
+        P = post.konversi(Q);
+        System.out.println("Posftix: " + P);
+    }
+}
