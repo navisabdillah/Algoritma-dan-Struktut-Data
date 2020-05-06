49b03b0f5332429ced5db898c6220e9536bc2796
navisabdillah <60720757+navisabdillah@users.noreply.github.com>
2020-05-06 18:12:49 +0700

    Add files via upload

---
+++ Pertemuan 13/Tugas/Tugas3/MainDaftarMahasiswa.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MainDaftarMahasiswa {
+    public static void main(String[] args) {
+         DaftarMahasiswa data = new DaftarMahasiswa();
+        Scanner sc = new Scanner(System.in);
+        String nama;
+        int nilai;
+        for(int i = 0; i < 5; i++){
+            System.out.print("Masukkan nama Mahasiswa\t\t: ");
+            nama = sc.nextLine();
+            System.out.print("Masukkan nilai Mahasiswa\t: ");
+            nilai = sc.nextInt();
+            sc.nextLine();
+            data.addLast(nama, nilai);
+            System.out.println("");
+        }
+        data.print();
+        data.SelectionSort();
+        data.print();
+    }
+    }
+
