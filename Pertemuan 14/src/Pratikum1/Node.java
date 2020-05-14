1508926b51707bc268f12b612fc6181bc59c997b
navisabdillah <60720757+navisabdillah@users.noreply.github.com>
2020-05-14 10:39:18 +0700

    Add files via upload

---
+++ Pertemuan 14/src/Pratikum1/Node.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pratikum1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Node {
+
+    int data;
+    Node left;
+    Node right;
+
+    public Node() {
+
+    }
+
+    public Node(int data) {
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
